84f37390d1f50df6bddaf8120b4012091823e7a8
Taniya <tanya.kalra200@gmail.com>
2023-07-13 16:41:48 +0530

    submitting code-Thu Jul 13 2023 16:41:45 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text,setText] = useState("");
+
+  function textRender(event){
+    setText(event.target.value);
+  }
+  
   return (
     <div>
         {/* Do not remove the main div */}
+        <div className="parent">
+          <div>
+            <h1>Parent Component</h1>
+            </div>
+            <p>{text}</p>
+          <div className="child">
+          <div>
+            <h1>Child Component</h1>
+            <input type="text" name="something" onChange={textRender} />
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
